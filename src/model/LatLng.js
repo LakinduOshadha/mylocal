7a7b26f4eef1629276d1b4ed12df721ae9c98359
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-05-20 15:54:39 +0530

    cleaned-up model/LatLng.js

--- src/model/LatLng.js
+++ src/model/LatLng.js
@@ -1,18 +1,5 @@
 import {arrayFlatten} from '../model/DataStructures.js';
 
-export function getTranform(
-  [width, height],
-  [minLat, minLng],
-  [latSpan, lngSpan],
-) {
-  return function([lat, lng]) {
-    return [
-      width * (lng - minLng) / lngSpan,
-      height * (1 - (lat - minLat) / latSpan),
-    ];
-  }
-}
-
 export function roundLatLng([lat, lng]) {
   const Q = 10 ** 6;
   const r = (x) => Math.round(x * Q) / Q;
@@ -61,22 +48,4 @@ export function getBounds(latLngList) {
     },
     [[180, 180], [-180, -180]],
   );
-}
-
-function deg2rad(deg) {
-  return deg * (Math.PI/180)
-}
-
-export function getDistance([lat1, lng1], [lat2, lng2]) {
-  var R = 6371;
-  var dLat = deg2rad(lat2 - lat1);
-  var dlng = deg2rad(lng2 - lng1);
-  var a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-    Math.sin(dlng / 2) * Math.sin(dlng / 2)
-    ;
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  var d = R * c;
-  return d;
 }
