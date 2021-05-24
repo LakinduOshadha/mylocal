24482c5967a3268c76f23e32d56f218a5683ddf3
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-05-24 08:01:50 +0530

    DetailedInfo - Pull basic info from GIGServer

--- src/model/DetailedInfo.js
+++ src/model/DetailedInfo.js
@@ -1,5 +1,18 @@
+import Entity, {ENTITY} from './Entity.js';
+import GIGServer from './GIGServer.js';
+
 export async function getSummary(entityID) {
-  return [
-    `Basic summary about ${entityID}`,
-  ].join(' ')
+  const entityType = Entity.getEntityType(entityID);
+  const entity = await GIGServer.getEntity(entityID);
+
+  switch(entityType) {
+    case ENTITY.PROVINCE:
+      return [
+        `The ${entity.name} Province is one of the nine Provinces in Sri Lanka. It has an area of ${entity.area}km², and a population of ${entity.population} (2012 census)`,
+      ].join(' ')
+    default:
+      return [
+        `Basic summary about ${entity.name}`,
+      ].join(' ')
+  }
 }
