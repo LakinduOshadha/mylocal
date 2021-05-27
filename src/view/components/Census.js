1d36cc0943444857c3b78237ca176f1c9ca8e64e
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-05-27 14:04:02 +0530

    Added model/Census.js

--- src/view/components/Census.js
+++ src/view/components/Census.js
@@ -1,5 +1,6 @@
 import GIGServer from 'model/GIGServer.js';
 import MathX from 'model/MathX.js';
+import {CENSUS_TABLES} from 'model/Census.js';
 import {
   titleCase,
   formatPercent,
@@ -8,33 +9,6 @@ import PieChart from 'view/charts/PieChart.js';
 import Pyramid from 'view/charts/Pyramid.js';
 
 import './Census.css';
-
-const CENSUS_TABLES = [
-  'ethnicity_of_population',
-  'religious_affiliation_of_population',
-  'gender_of_population',
-  'age_group_of_population',
-  'relationship_to_household_head_of_population',
-  'communication_items_owned_by_household',
-  'cooking_fuel_of_household',
-  'roof_type_in_housing_unit',
-  'rooms_in_housing_unit',
-  'floor_type_in_housing_unit',
-  'solid_waste_disposal_by_household',
-  'source_of_drinking_water_of_household',
-  'households_living_in_housing_unit',
-  'structure_of_housing_units',
-  'housing_ownership_status_of_household',
-  'toilet_facilities_of_household',
-  'lighting_of_household',
-  'living_quarters',
-  'type_of_housing_unit',
-  'marital_status_of_population',
-  // 'wall_type_in_housing_units',
-  // 'occupation_status_of_housing_units',
-  'year_of_construction_of_housing_unit',
-  // 'persons_living_in_housing_unit',
-];
 
 const ageKeysMap = {
   age_group_of_population: [
