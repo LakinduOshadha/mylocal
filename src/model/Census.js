d89ec6a36ab1a8acfd1eb4fe229012a822e1f8c5
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-04 15:34:10 +0530

    #FEATURE-MINOR Changed order of census info

--- src/model/Census.js
+++ src/model/Census.js
@@ -1,30 +1,36 @@
 import Format from 'view/Format.js';
 
 export const CENSUS_TABLES = [
-  'ethnicity_of_population',
-  'religious_affiliation_of_population',
   'gender_of_population',
   'age_group_of_population',
-  'relationship_to_household_head_of_population',
+  'marital_status_of_population',
+
   'communication_items_owned_by_household',
-  'cooking_fuel_of_household',
-  'roof_type_in_housing_unit',
-  'rooms_in_housing_unit',
-  'floor_type_in_housing_unit',
-  'solid_waste_disposal_by_household',
-  'source_of_drinking_water_of_household',
-  'households_living_in_housing_unit',
-  'structure_of_housing_units',
-  'housing_ownership_status_of_household',
-  'toilet_facilities_of_household',
   'lighting_of_household',
+  'cooking_fuel_of_household',
+
+  'source_of_drinking_water_of_household',
+  'solid_waste_disposal_by_household',
+  'toilet_facilities_of_household',
+
+  'roof_type_in_housing_unit',
+  'floor_type_in_housing_unit',
+  'wall_type_in_housing_units',
+  'structure_of_housing_units',
+
   'living_quarters',
   'type_of_housing_unit',
-  'marital_status_of_population',
-  'wall_type_in_housing_units',
   'occupation_status_of_housing_units',
   'year_of_construction_of_housing_unit',
+
+  'rooms_in_housing_unit',
   'persons_living_in_housing_unit',
+  'relationship_to_household_head_of_population',
+  'households_living_in_housing_unit',
+  'housing_ownership_status_of_household',
+
+  'ethnicity_of_population',
+  'religious_affiliation_of_population',
 ];
 
 export const CENSUS_TABLE_SPAN_INFO = {
